b847ff4f8c311b599361cc1475bcdb737187c6b9
陳閔亭 <chenminting@chenmintingdeMacBook-Air.local>
2020-10-18 13:31:34 +0800

    add filitering

--- read.py
+++ read.py
@@ -14,3 +14,13 @@ for d in data:
  	print(sum_len)
 print('檔案讀取完了，總共有',len(data),'資料')
 print('留言的平均長度是', sum_len/len(data))
+
+#篩選
+new =[]
+for d in data:
+#for loop 的意思就是把data內的一個一個拿出來
+    if len(d) < 100:
+    	new.append(d)
+print ('一共有', len(new), '筆留言長度小於100')
+#前面沒空格，不在for內，才不會一直print)
+print(new[0])
