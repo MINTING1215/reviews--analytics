661709a5ee0edd59e9d238b4f33d8bacee9cbf49
陳閔亭 <chenminting@chenmintingdeMacBook-Air.local>
2021-08-19 12:18:19 +0800

    add word count

--- read.py
+++ read.py
@@ -1,18 +1,17 @@
 data = []
 count = 0
 with open ('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
-		count += 1
-		if count %1000 == 0:
-		    print(len(data))
-
+    for line in f:
+        data.append(line)
+        count += 1
+        if count %1000 == 0:
+            print(len(data))
+print('檔案讀取完了，總共有',len(data),'資料')
+print(data[0])
 
 sum_len = 0
 for d in data:
- 	sum_len = sum_len + len (d)
- 	print(sum_len)
-print('檔案讀取完了，總共有',len(data),'資料')
+     sum_len = sum_len + len (d)
 print('留言的平均長度是', sum_len/len(data))
 
 #篩選
@@ -20,7 +19,7 @@ new =[]
 for d in data:
 #for loop 的意思就是把data內的一個一個拿出來
     if len(d) < 100:
-    	new.append(d)
+        new.append(d)
 print ('一共有', len(new), '筆留言長度小於100')
 #前面沒空格，不在for內，才不會一直print)
 print(new[0])
@@ -28,6 +27,38 @@ print(new[0])
 good = []
 for d in data:
     if 'good' in d:
-    	good.append(d)
+        good.append(d)
 print('一共有 ', len(good), '筆留言提到good')
 print(good[0])
+
+#文字計數
+
+wc = {} # word_count
+for d in data:
+    words = d.split(' ')
+    for word in words:
+        if word in wc: 
+            wc[word] += 1
+        else:
+            wc[word] = 1 #新增新的key進wc字典
+
+for word in wc:
+    if wc[word] > 1000000 :
+        print(word, wc[word]) 
+
+print(len(wc))
+
+while True :
+    word = input ('請問你想查什麼字？') 
+    if word == 'q':
+    	break
+    if word in wc:
+        print(word, '出現過的次數為', wc[word])
+    else:
+    	print('這個字沒有出現過')
+
+print('感謝使用本查詢功能')
+    
+
+
+
